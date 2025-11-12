126079f7e7708817957a7a3b64240c537cc6f25d
user <koha1739@mail.ru>
2025-11-12 18:21:28 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Genre_analysis_frontend/sw.js', { scope: '/Genre_analysis_frontend/' })})}
